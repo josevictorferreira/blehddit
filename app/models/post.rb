708ea789f839a4a3e9d3f7b278a997a7b0f8b2d1
José Victor <josevictorferreira@outlook.com>
2017-01-19 23:07:37 -0200

    Added link thumbnail to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
 
+  before_save :get_link_thumbnailer
+
+  def get_link_thumbnailer
+    unless link.blank?
+      img = LinkThumbnailer.generate(self.link).images.first.src
+      self.image = img unless img.nil?
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117231107) do
+ActiveRecord::Schema.define(version: 20170119235918) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170117231107) do
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
