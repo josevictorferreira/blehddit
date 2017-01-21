722fc9b5b044669684c8c62e9ea00b0a1ebd756e
José Victor <josevictorferreira@outlook.com>
2017-01-21 18:22:39 -0200

    Improved the layout of posts page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,19 @@
 class Post < ApplicationRecord
 
-  before_save :get_link_thumbnailer
+  before_save :get_link_thumbnailer, :set_votes_to_0
 
   def get_link_thumbnailer
     unless link.blank?
       img = LinkThumbnailer.generate(self.link).images.first.src
       self.image = img unless img.nil?
     end
+  end
+
+  def set_votes_to_0
+    self.upvotes, self.downvotes = 0, 0
+  end
+
+  def votes
+    return self.upvotes - self.downvotes
   end
 end
